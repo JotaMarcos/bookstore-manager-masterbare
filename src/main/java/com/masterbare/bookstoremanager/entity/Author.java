b90537f151066f690926e0b74683e3f4f1768e08
João Marcos <jmarcos_duarte@hotmail.com>
2022-12-20 13:09:26 -0400

    Developed project entities

---
+++ src/main/java/com/masterbare/bookstoremanager/entity/Author.java
@@ -0,0 +1,28 @@
+package com.masterbare.bookstoremanager.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data // Adiciona os métodos getters, setters, equals e hashcode
+@Builder // Padrões de projetos para a construção de objetos de forma segura
+@NoArgsConstructor // Construtores sem argumentos
+@AllArgsConstructor // Construtores com argumentos
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false)
+    private Integer age;
+
+
+}
